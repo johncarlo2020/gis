3522bce6ed13da4b529458da3db4f3e65a786fea
Lordanniel23 <lordanniel@gmail.com>
2022-12-30 21:58:14 +0800

    group user filter

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+// users 
+use App\Http\Controllers\userAdmin;
+use App\Http\Controllers\userEncoder;
+use App\Http\Controllers\userRegistrar;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,18 +29,31 @@ Auth::routes();
 Route::group(['middleware' => 'auth'], function (){
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+// =================================================================================================================================
 //admin routes
 Route::get('userAdmin/index', [HomeController::class, 'AdminHome'])->name('admin.home')->middleware('userAdmin');
-Route::get('userAdmin/user', [UserController::class, 'index'])->name('admin.user')->middleware('userAdmin');
 
+Route::group(['middleware' => ['userAdmin']], function() {
+        Route::get('userAdmin/user', [UserController::class, 'index'])->name('admin.user');
+});
 
+// =================================================================================================================================
 //encoder routes
 Route::get('userRegistrar/index', [HomeController::class, 'RegistrarHome'])->name('registrar.home')->middleware('userEncoder');
 
+Route::group(['middleware' => ['userAdmin']], function() {
+        Route::get('userAdmin/user', [UserController::class, 'index'])->name('admin.user');
+});
+
+// =================================================================================================================================
 //registrar routes
 Route::get('userEncoder/index', [HomeController::class, 'EncoderHome'])->name('encoder.home')->middleware('userRegistrar');
 
-
+Route::group(['middleware' => ['userAdmin']], function() {
+        Route::get('userAdmin/user', [UserController::class, 'index'])->name('admin.user');
+});
+// =================================================================================================================================
 
 });
+
+
