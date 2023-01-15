1193fdb9a1f53b86364ec0e65e8069574ff52602
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2023-01-15 15:25:18 +0800

    alteration

---
+++ database/migrations/2023_01_15_072413_make_mname_nullable_on_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeMnameNullableOnUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->dropColumn("mname");
+
+        });
+
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->string('mname')->nullable()->after("fname");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
